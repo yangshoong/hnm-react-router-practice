5553bbf69f39495703f81c56135b5e796a678bec
yangshoong <crinaeae83@gmail.com>
2024-03-28 01:43:31 +0900

    H&M page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
-
-
+import ProductAll from './page/ProductAll';
+import ProductDetail from './page/ProductDetail';
+import Login from './page/Login';
+import Navbar from './component/Navbar';
 
 
 function App() {
   return (
     <div>
-
-
-      
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<ProductAll/> } />
+        <Route path="/login" element={<Login/> } />
+        <Route path="/product/:id" element={<ProductDetail/> } />
+      </Routes>
     </div>
   );
 }
