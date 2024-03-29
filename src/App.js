5fea5435bb3e8245f117aed955698d3ef1929a92
Yangshoong <crinaeae83@gmail.com>
2024-03-29 12:50:41 +0900

    homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import ProductAll from './page/ProductAll';
-// import ProductDetail from './page/ProductDetail';
 import Login from './page/Login';
 import Navbar from './component/Navbar';
 import PrivateRoute from "./route/PrivateRoute";
