01e6e9d02c32f56bc4d46f1b7fc2899c61ad3309
yangshoong <crinaeae83@gmail.com>
2024-03-30 09:40:51 +0900

    product detail

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,6 +5,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
+
 const Navbar = () => {
     const menuList = [
         '여성',
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+
 
 const ProductDetail = () => {
     let { id } = useParams();
@@ -25,12 +26,30 @@ const ProductDetail = () => {
     return (
         <Container>
             <Row>
-                <Col>
+                <Col calssName="product-img">
                     <img src={product?.img} alt="" />
                 </Col>
                 <Col>
-                    <div>{product?.title}</div>
-                    <div>{product?.price}</div>
+                    <div>
+                        <div>{product?.title}</div>
+                        <div>{product?.price}</div>
+                    </div>
+                    <div>
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </div>
+
+                    <div>
+                        <div className="d-grid gap-2">
+                            <Button variant="primary" size="lg">
+                                Block level button
+                            </Button>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         </Container>
