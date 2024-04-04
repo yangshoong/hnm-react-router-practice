79b35ff0305fc14d2b2bafcc5cc00ac39e486cf4
yangshoong <crinaeae83@gmail.com>
2024-04-05 02:24:00 +0900

    redux toolkit

--- src/redux/Slices/productSlice.js
+++ src/redux/Slices/productSlice.js
@@ -28,19 +28,6 @@ export const fetchProductDetail = createAsyncThunk('productDetail/fetch', async 
   }
 });
 
-// function productReducer(state = initialState, action) {
-//   let { type, payload } = action;
-//   switch (type) {
-//     case "GET_PRODUCT_SUCCESS":
-//       return { ...state, productList: payload.data };
-//     case "GET_SINGLE_PRODUCT_SUCCESS":
-//       return {...state, selectedItem: payload.data};
-//     default:
-//       return { ...state };
-//   }
-// }
-// export default productReducer;
-
 const productSlice = createSlice({
   name: "product",
   initialState,
